2a7a8cce2a692841748d7a533061a7c1089c6a6a
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-12-14 15:21:07 +0200

    Initial commit

---
+++ UnitTest10.1/UnitTest10.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab10.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest101
+{
+	TEST_CLASS(UnitTest101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = ProcessTXT("../t.txt");
+			Assert::AreEqual(t, 8);
+		}
+	};
+}
